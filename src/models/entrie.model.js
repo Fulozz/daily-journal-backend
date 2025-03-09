9893cdb304942ba46ad36727672c650a3166208e
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-03-09 20:18:46 -0300

    fix: remove data field from entrie model

--- src/models/entrie.model.js
+++ src/models/entrie.model.js
@@ -10,7 +10,6 @@ const Schema = mongoose.Schema;
 const entrieSchema = new Schema({
     description: { type: String, required: true},
     title: { type: String, required: true},
-    data: { type: Object},
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 },{
     timestamps: true,
