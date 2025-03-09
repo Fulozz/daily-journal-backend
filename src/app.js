6b46f0e57a9db31a028307edfd35baf6b905ba52
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-03-09 17:47:38 -0300

    fix: update mongoose connection import path in app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express()
 
 // => Conexão com o banco de dados
-const mongooseConnection = require('./database/mongooseConection.config');
+const mongooseConnection = require('./configuration/mongooseConection.config');
 
 
 // => importe de rotas API
