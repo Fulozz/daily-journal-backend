3c29985fb624b4ea8210943852b7a56c6cf151ea
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-03-09 18:30:22 -0300

    fix: correct import order in task.routes.js

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const tasksController = require('../controllers/task.controller')
 const auth  = require('../middleware/auth.middleware')
 
-const tasksController = require('../controllers/task.controller')
+
 
 // ==> Rota responsavel por criar uma nova 'Task': (POST) localhost:3000/api/v1/tasks
 router.post('/tasks', auth, tasksController.createTask);
@@ -16,3 +16,5 @@ router.get('/tasks/:taskId', auth, tasksController.returnTaskById);
 
 // ==> Rota responsavel por atualizar uma 'Task' pelo 'Id': (PUT) localhost:3000/api/v1/tasks/:taskId
 router.put('/tasks/:taskId', auth, tasksController.updateTask);
+
+module.exports = router;
