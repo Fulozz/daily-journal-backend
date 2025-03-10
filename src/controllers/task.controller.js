19463280f8f9df4adf5a6186aa0760003d443e33
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-03-10 17:39:20 -0300

    delete by req.body

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -68,8 +68,8 @@ exports.updateTask = async (req, res) => {
 
 exports.deleteTask = async (req, res) => {
     try {
-        const { taskId } = req.params;
-        const task = await Task.findOneAndDelete({ _id: taskId, user: req.userData._id });
+        const { taskId, userId } = req.body;
+        const task = await Task.findOneAndDelete({ _id: taskId, userId: userId });
         if (!task) {
             return res.status(404).json({ message: 'Task not found!' });
         }
