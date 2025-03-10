8bb499d688501ad484969ce0642a26aa31304bcd
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-03-10 17:36:00 -0300

    delete API connection

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -17,4 +17,8 @@ router.get('/tasks/:taskId', auth, tasksController.returnTaskById);
 // ==> Rota responsavel por atualizar uma 'Task' pelo 'Id': (PUT) localhost:3000/api/v1/tasks/:taskId
 router.patch('/tasks/:taskId', auth, tasksController.updateTask);
 
+
+// ==> Rota responsavel por deletar uma 'Task' pelo 'Id': (DELETE) localhost:3000/api/v1/tasks/:taskId
+router.delete('/tasks/:taskId', auth, tasksController.deleteTask);
+
 module.exports = router;
