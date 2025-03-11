798e65c03e5c63389c329cbffdddd0d4b2cf83de
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-03-10 22:48:36 -0300

    fix: normalize authorization header casing to lowercase

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 module.exports = (req, res, next)=>{
-    const authHeader = req.headers['Authorization'];
+    const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if(token == null) return res.sendStatus(401); // Unauthorized
