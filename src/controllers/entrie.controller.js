859a0c604ed372684f091f84f2deba60f4358eb8
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2025-03-11 00:10:14 -0300

    fix: update deleteEntrie to correctly find and delete entrie using userId from request data

--- src/controllers/entrie.controller.js
+++ src/controllers/entrie.controller.js
@@ -59,12 +59,17 @@ exports.updateEntrie = async (req, res) => {
 
 exports.deleteEntrie = async (req, res) => {
     try {
-        const { entrieId } = req.params;
-        const entrie = await Entrie.findOneAndDelete ({ _id: entrieId, user: req.userId });
+        const userId = req.userData._id;
+        const  entrieId = req.params.entrieId;
+        const entrie = await Entrie.findOne({ _id: entrieId, user: userId });
         if (!entrie) {
             return res.status(404).json({ message: 'Entrie not found!' });
         }
-        res.status(200).json({ message: 'Entrie deleted successfully!', entrie });
+
+        const entrieDelete = await entrie.findOneAndDelete({ _id: entrieId, userId: userId});
+        if(entrieDelete){
+            return res.status(200).json({ message: 'Entrie deleted successfully!', entrie });
+        }
     }
     catch (error) {
         res.status(400).json({ error: error.message });
